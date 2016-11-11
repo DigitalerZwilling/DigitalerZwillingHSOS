570c65ca55882729a0c15f86f40ca434933cbd5b
chrklaas <chklaas@gmx.de>
2016-11-11 15:34:11 +0100

    hubquer

--- DigitalerZwilling/src/java/Cache/HubquerpodestCache.java
+++ DigitalerZwilling/src/java/Cache/HubquerpodestCache.java
@@ -17,6 +17,7 @@ public class HubQuerPodestCache extends Cache{
 
     @Override
     public void update() {
+        //--------------------------------------------------------------
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
