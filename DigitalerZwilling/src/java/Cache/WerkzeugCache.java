a5cdf49462bc51e36c5b5a4eecb82f8efe77d6e4
AlexKeme <alex.k1@web.de>
2016-11-14 16:40:46 +0100

    werkzeugcache implementiert
    + klasse werkzeug hinzugefügt

--- DigitalerZwilling/src/java/Cache/WerkzeugCache.java
+++ DigitalerZwilling/src/java/Cache/WerkzeugCache.java
@@ -5,8 +5,14 @@
  */
 package Cache;
 
+import DatenKlassen.Element;
+import DatenKlassen.Werkzeug;
+import DatenbankSchnittestelle.Datenbankschnittstelle;
 import java.sql.ResultSet;
+import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -16,14 +22,62 @@ public class WerkzeugCache extends Cache{
 
     @Override
     public void update() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Map<String,List<String>> rsMap= Datenbankschnittstelle.getInstance().datenbankAnfrage("SELECT id_werkzeug,zeitstempel,user_parameter,zustand,id_roboter from Werkzeug");
+        List<String> ids_w = rsMap.get("id_werkzeug");
+        List<String> zeitstempel = rsMap.get("zeitstempel");
+        List<String> user_parameter = rsMap.get("user_parameter");
+        List<String> zustand = rsMap.get("zustand");
+        List<String> ids_r = rsMap.get("id_roboter");
+        Werkzeug werkzeug;
+        for (int i=0;i<ids_w.size();i++){
+            werkzeug=(Werkzeug)(state==true?elements[0].get(Long.getLong(ids_w.get(i))):elements[1].get(Long.getLong(ids_w.get(i))));                 //andersrum als bei getById
+            werkzeug.setZeitstempel(LocalTime.parse(zeitstempel.get(i))); // Ueberpruefen
+            werkzeug.setUser_Parameter(user_parameter.get(i));
+            werkzeug.setZustand(Integer.valueOf(zustand.get(i)));
+            werkzeug.setRoboterID(Long.valueOf(ids_r.get(i)));
+        }
     }
 
     @Override
     public void updateAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Map<Long,Element> allWerkzeug1=new HashMap<>();
+        Map<Long,Element> allWerkzeug2=new HashMap<>();
+        
+        Map<String,List<String>> rsMap= Datenbankschnittstelle.getInstance().datenbankAnfrage("SELECT id_werkzeug,bezeichnung,zeitstempel,user_parameter,zustand,id_roboter from Werkzeug");
+        List<String> ids_w = rsMap.get("id_werkzeug");
+        List<String> bezeichnung = rsMap.get("bezeichnung");
+        List<String> zeitstempel = rsMap.get("zeitstempel");
+        List<String> user_parameter = rsMap.get("user_parameter");
+        List<String> zustand = rsMap.get("zustand");
+        List<String> ids_r = rsMap.get("ids_r");
+        
+        Werkzeug werkzeug1,werkzeug2;
+        for (int i=0;i<ids_w.size();i++){
+            werkzeug1=new Werkzeug(Long.getLong(ids_w.get(i)),bezeichnung.get(i),user_parameter.get(i),LocalTime.parse(zeitstempel.get(i)),Integer.valueOf(zustand.get(i)));
+            werkzeug2=new Werkzeug(Long.getLong(ids_w.get(i)),bezeichnung.get(i),user_parameter.get(i),LocalTime.parse(zeitstempel.get(i)),Integer.valueOf(zustand.get(i)));
+            
+            werkzeug1.setRoboterID(this.readRoboter(werkzeug1.getId()));
+            werkzeug2.setRoboterID(this.readRoboter(werkzeug2.getId()));
+            
+            allWerkzeug1.put(werkzeug1.getId(),(werkzeug1));
+            allWerkzeug2.put(werkzeug2.getId(),(werkzeug2));
+        }
+        Map<Long,Element>[] m = new Map[2];
+        m[0]=allWerkzeug1;
+        m[1]=allWerkzeug2;
+        this.setElements(m);
     }
 
+    Long readRoboter(Long id){
+        Map<String,List<String>> rsMap = Datenbankschnittstelle.getInstance().datenbankAnfrage("SELECT roboter_id from Werkzeug where id_werkzeug="+id+" ");
+        List<String> ids = rsMap.get("id_roboter");
+        Long r_ids=null;
+        for (String s : ids){
+            r_ids=Long.getLong(s);
+        }
+        return r_ids;
+    }
+    
     private static WerkzeugCache instance;
 
     public static synchronized Cache getInstance(){
