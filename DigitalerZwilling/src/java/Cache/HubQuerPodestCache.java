ec27bc7760d23804f29332f25979bf8244ab25a7
iPorre <florian@83ade86d.funky.hs-osnabrueck.de>
2016-11-18 10:20:54 +0100

    HubQuerPodestCache
    SQL Abfragen müssen noch geprüft werden.

--- DigitalerZwilling/src/java/Cache/HubQuerPodestCache.java
+++ DigitalerZwilling/src/java/Cache/HubQuerPodestCache.java
@@ -5,10 +5,12 @@
  */
 package Cache;
 
-import DatenKlassen.Element;
 import DatenKlassen.HubQuerPodest;
-import java.sql.ResultSet;
+import DatenbankSchnittestelle.Datenbankschnittstelle;
+import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -19,12 +21,44 @@ public class HubQuerPodestCache extends Cache{
     @Override
     public void update() {
         //--------------------------------------------------------------
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Map<String,List<String>> rsMap = Datenbankschnittstelle.getInstance().datenbankAnfrage("SELECT id_Hubquerpodest, user_parameter, motor, oben, mittig, unten, zeitstempel form Hubquerpodest");
+        List<String> id = rsMap.get("id_Hubquerpodest");
+        List<String> userParameter = rsMap.get("user_parameter");
+        List<String> motor = rsMap.get("motor");
+        List<String> oben = rsMap.get("oben");
+        List<String> mittig = rsMap.get("mittig");
+        List<String> unten = rsMap.get("unten");
+        List<String> zeitstempel = rsMap.get("zeitstempel");
+        for(int i = 0; i<id.size();i++){
+            HubQuerPodest huQu = (HubQuerPodest) (state==true?elements[0].get(Long.getLong(id.get(i))):elements[0].get(Long.getLong(id.get(i))));
+            huQu.setUser_Parameter(userParameter.get(i));
+            huQu.setMotor(Boolean.getBoolean(motor.get(i)));
+            huQu.setOben(Boolean.getBoolean(oben.get(i)));
+            huQu.setMittig(Boolean.getBoolean(mittig.get(i)));
+            huQu.setUnten(Boolean.getBoolean(unten.get(i)));
+            huQu.setZeitstempel(LocalTime.parse(zeitstempel.get(i)));
+        }
     }
 
     @Override
     public void updateAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Map<String,List<String>> rsMap = Datenbankschnittstelle.getInstance().datenbankAnfrage("SELECT id_Hubquerpodest, user_parameter, motor, oben, mittig, unten, zeitstempel, bezeichnung, id_sektor form Hubquerpodest");
+        List<String> id = rsMap.get("id_Hubquerpodest");
+        List<String> userParameter = rsMap.get("user_parameter");
+        List<String> motor = rsMap.get("motor");
+        List<String> oben = rsMap.get("oben");
+        List<String> mittig = rsMap.get("mittig");
+        List<String> unten = rsMap.get("unten");
+        List<String> zeitstempel = rsMap.get("zeitstempel");
+        List<String> sektorId = rsMap.get("sektor_id");
+        List<String> bezeichnung = rsMap.get("bezeichnung");
+        for(int i = 0; i<id.size();i++){
+            elements[0] = new HashMap<>();
+            elements[1] = new HashMap<>();
+            elements[0].put(Long.getLong(id.get(i)), new HubQuerPodest(Boolean.getBoolean(motor.get(i)), Boolean.getBoolean(oben.get(i)), Boolean.getBoolean(mittig.get(i)), Boolean.getBoolean(unten.get(i)), Long.getLong(sektorId.get(i)), Long.getLong(id.get(i)), bezeichnung.get(i), userParameter.get(i), LocalTime.parse(zeitstempel.get(i))));
+            elements[1].put(Long.getLong(id.get(i)), new HubQuerPodest(Boolean.getBoolean(motor.get(i)), Boolean.getBoolean(oben.get(i)), Boolean.getBoolean(mittig.get(i)), Boolean.getBoolean(unten.get(i)), Long.getLong(sektorId.get(i)), Long.getLong(id.get(i)), bezeichnung.get(i), userParameter.get(i), LocalTime.parse(zeitstempel.get(i))));
+        }
+        updateGruppenIds();
     }
 
     private static HubQuerPodestCache instance;
@@ -36,7 +70,15 @@ public class HubQuerPodestCache extends Cache{
         return instance;
     }
    
-    //.
+    private void updateGruppenIds(){
+        Map<String,List<String>> rsMap = Datenbankschnittstelle.getInstance().datenbankAnfrage("SELECT id_Hubquerpodest1, id_hubquerpodest2 from Hubquerpodest_Hubquerpodest");
+        List<String> id1 = rsMap.get("id_Hubquerpodest1");
+        List<String> id2 = rsMap.get("id_Hubquerpodest2");
+        for(int i=0;i<id1.size();i++){
+            HubQuerPodest podest = HubQuerPodest.class.cast(elements[0].get(Long.getLong(id1.get(i))));
+            podest.getGruppenIDs().add(Long.getLong(id2.get(i)));
+        }
+    }
     
     
 }
