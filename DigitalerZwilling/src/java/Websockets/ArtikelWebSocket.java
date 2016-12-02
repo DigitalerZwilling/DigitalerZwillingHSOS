ff2820cbdca11d9e5d63bd61d5522428807dc309
chrklaas <chklaas@gmx.de>
2016-12-02 15:15:05 +0100

    WebSockets
    allerdings noch mit toString statt toJson

---
+++ DigitalerZwilling/src/java/Websockets/ArtikelWebSocket.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Websockets;
+
+import Cache.ArtikelCache;
+import Websockets.SessionRegister.ArtikelSessionRegister;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ *
+ * @author user
+ */
+@ServerEndpoint("/ArtikelWebSocket")
+public class ArtikelWebSocket extends WebSocketConfig{
+  @Inject ArtikelSessionRegister artikelSessionRegister;
+  //private WebSocketConfig webSocketConfig;
+  
+  @OnMessage
+  public void messageReceiver(String message) {
+      System.out.println(message);
+      this.setId(Long.parseLong(message));
+      this.setKlasseninfo("Artikel");
+      this.artikelSessionRegister.addSession(this);
+      this.fertigRegistriert();
+      
+      /*try {
+          //System.out.println("Received message:" + message);
+          session.getBasicRemote().sendObject(aCache.getById(1L));
+          System.out.println("onMessage: " + aCache.getById(1L).getBezeichnung());
+          aCache.update();
+      } catch (IOException ex) {
+          Logger.getLogger(ArtikelWebSocket.class.getName()).log(Level.SEVERE, null, ex);
+      /*} catch (EncodeException ex) {
+          Logger.getLogger(ArtikelWebSocket.class.getName()).log(Level.SEVERE, null, ex);
+      */
+  }
+
+  @OnOpen
+  public void onOpen(Session session) {
+    //this.session=session;
+    //this.session=session;
+    
+    this.setSession(session);
+    System.out.println("onOpen: " + session.getId());
+    //this.messageReceiver("hi");
+    
+    //System.out.println("onOpen: Notification list size: " + sessions.size());
+  }
+ 
+    /**
+     * The user closes the connection.
+     * 
+     * Note: you can't send messages to the client from this method
+     */
+    @OnClose
+    public void onClose(Session session){
+        this.nichtmehrRegistriert();
+        this.artikelSessionRegister.remove(this);
+        System.out.println("Session " +session.getId()+" has ended");
+    }
+    
+}
---
+++ DigitalerZwilling/src/java/Websockets/WebSocketConfig.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Websockets;
+
+import java.util.Objects;
+import javax.websocket.Session;
+
+/**
+ *
+ * @author user
+ */
+public class WebSocketConfig {
+    private Session session;
+    private Long id;
+    private String klasseninfo;
+    private Boolean istListe;
+    
+    private Boolean registriert;
+
+    public WebSocketConfig() {
+        this.istListe = Boolean.FALSE;
+        this.registriert = Boolean.FALSE;
+        System.out.println("config hier!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+
+    protected void fertigRegistriert(){
+        this.registriert=Boolean.TRUE;
+    }
+    protected void nichtmehrRegistriert(){
+        this.registriert=Boolean.FALSE;
+    }
+    public Boolean istRegistriert(){
+        return this.registriert;
+    }
+    
+    public Boolean getIstListe() {
+        return istListe;
+    }
+
+    public void setIstListe(Boolean istListe) {
+        this.istListe = istListe;
+    }
+    
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKlasseninfo() {
+        return klasseninfo;
+    }
+
+    public void setKlasseninfo(String klasseninfo) {
+        this.klasseninfo = klasseninfo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.session);
+        hash = 53 * hash + Objects.hashCode(this.id);
+        hash = 53 * hash + Objects.hashCode(this.klasseninfo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final WebSocketConfig other = (WebSocketConfig) obj;
+        if (!Objects.equals(this.klasseninfo, other.klasseninfo)) {
+            return false;
+        }
+        if (!Objects.equals(this.session, other.session)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
