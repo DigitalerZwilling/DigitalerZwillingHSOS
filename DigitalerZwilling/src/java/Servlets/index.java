d5cb42e1eabd3e26584e954c24f1d73f062b78e3
AlexKeme <alex.k1@web.de>
2016-12-09 16:15:12 +0100

    serverausgabe erweitert

--- DigitalerZwilling/src/java/Servlets/index.java
+++ DigitalerZwilling/src/java/Servlets/index.java
@@ -5,13 +5,18 @@
  */
 package Servlets;
 
-
 import Cache.ArtikelCache;
+import Cache.GelenkCache;
+import Cache.HubPodestCache;
+import Cache.HubQuerPodestCache;
+import Cache.RoboterCache;
+import Cache.SektorCache;
+import Cache.SensorCache;
+import Cache.TransportbandCache;
+import Cache.WarentraegerCache;
+import Cache.WerkzeugCache;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -24,7 +29,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class index extends HttpServlet {
 
-
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -34,8 +38,27 @@ public class index extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Inject ArtikelCache a; 
-    @Inject ArtikelCache b; 
+    @Inject
+    ArtikelCache artikelCache;
+    @Inject
+    GelenkCache gelenkCache;
+    @Inject
+    HubPodestCache hubPodestCache;
+    @Inject
+    HubQuerPodestCache hubQuerPodestCache;
+    @Inject
+    RoboterCache roboterCache;
+    @Inject
+    SektorCache sektorCache;
+    @Inject
+    SensorCache sensorCache;
+    @Inject
+    TransportbandCache transportbandCache;
+    @Inject
+    WarentraegerCache warentraegerCache;
+    @Inject
+    WerkzeugCache werkzeugCache;
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -44,14 +67,80 @@ public class index extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet test</title>");   
+            out.println("<title>Server Ausgabe</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet test at " + request.getContextPath() + "</h1>");
-            out.println(a.toString());
-            //a.update();
-            out.println("<br>");
-            out.println(b.toString());
+
+            //Artikel
+            out.println("<h1>Artikel</h1>");
+            this.artikelCache.getAll().forEach((e) -> {
+                out.println(e.toJson());
+            });
+            out.println("<br><br>");
+
+            //Gelenk
+            out.println("<h1>Gelenk</h1>");
+            this.gelenkCache.getAll().forEach((e) -> {
+                out.println(e.toJson());
+            });
+            out.println("<br><br>");
+
+            //HubPodest
+            out.println("<h1>HubPodest</h1>");
+            this.hubPodestCache.getAll().forEach((e) -> {
+                out.println(e.toJson());
+            });
+            out.println("<br><br>");
+
+            //HubQuerPodest
+            out.println("<h1>HubQuerPodest</h1>");
+            this.hubQuerPodestCache.getAll().forEach((e) -> {
+                out.println(e.toJson());
+            });
+            out.println("<br><br>");
+
+            //Roboter
+            out.println("<h1>Roboter</h1>");
+            this.roboterCache.getAll().forEach((e) -> {
+                out.println(e.toJson());
+            });
+            out.println("<br><br>");
+
+            //Sektor
+            out.println("<h1>Sektor</h1>");
+            this.sektorCache.getAll().forEach((e) -> {
+                out.println(e.toJson());
+            });
+            out.println("<br><br>");
+
+            //Sensor
+            out.println("<h1>Sensor</h1>");
+            this.sensorCache.getAll().forEach((e) -> {
+                out.println(e.toJson());
+            });
+            out.println("<br><br>");
+
+            //Transportband
+            out.println("<h1>Transportband</h1>");
+            this.transportbandCache.getAll().forEach((e) -> {
+                out.println(e.toJson());
+            });
+            out.println("<br><br>");
+
+            //Warentraeger
+            out.println("<h1>Warentraeger</h1>");
+            this.warentraegerCache.getAll().forEach((e) -> {
+                out.println(e.toJson());
+            });
+            out.println("<br><br>");
+
+            //Werkzeug
+            out.println("<h1>Werkzeug</h1>");
+            this.werkzeugCache.getAll().forEach((e) -> {
+                out.println(e.toJson());
+            });
+            out.println("<br><br>");
+
             out.println("</body>");
             out.println("</html>");
         }
